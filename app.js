0fe05693ae22a4fca4f7a8d27ac6ba32330872d7
AnithaPiske <anithasangameshwar444@gmail.com>
2025-01-30 15:21:24 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ <div id=parent1> 
+    <div id=child1> 
+        <h1>heading1</h1>
+        <h2>heading2</h2>
+    </div>
+    <div id=child2> 
+        <h1>heading1</h1>
+        <h2>heading2</h2>
+    </div>
+ </div>
+ * 
+ */
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "heading1"), React.createElement("h2", {}, "heading2")])
+
+        , React.createElement("div", { id: "child2" },
+            [React.createElement("h1", {}, "heading1"), React.createElement("h2", {}, "heading2")])
+    ]
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
